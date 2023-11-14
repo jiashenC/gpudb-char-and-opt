0395d20abdc50ae07f3d90e9dc4ddc18c2759e29
Jiashen Cao <caojiashen24@gmail.com>
2023-11-14 10:53:46 -0500

    move crystal-opt as a submodule

--- crystal/crystal-opt_src/test/ssb/queries/transformed/q11.sql
+++
@@ -1,6 +0,0 @@
-select sum(lo_extendedprice * lo_discount) as revenue
-from lineorder
-where lo_orderdate >= 19930101 and lo_orderdate <= 19940101 and lo_discount>=1
-and lo_discount<=3
-and lo_quantity<25;
-
